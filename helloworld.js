e8a71c867a8f4df66ded2bc19e00fb9fb498cea6
dadiestfish2077 <71837491+dadiestfish2077@users.noreply.github.com>
2021-10-09 19:21:33 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hi!!!\n')
+   response.end('Hey\n')
 }).listen(3333)
 
 // Console will print the message
